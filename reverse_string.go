7ebc93b404cb4bda2e0290af9f77690b81f5e5c9
NataliiaMed <nataliasharya@gmail.com>
2022-05-18 21:59:25 +0400

    4.1-6

--- reverse_string.go
+++ reverse_string.go
@@ -3,7 +3,7 @@ package reverse_string
 func ReverseString(input string) (output string) {
 	// solution goes here
 	n := []rune(input)
-	m := make([]rune, len(input))
+	m := make([]rune, len(n))
 	for i, s := 0, len(n)-1; i != len(n); i, s = i+1, s-1 {
 		m[i] = n[s]
 	}
